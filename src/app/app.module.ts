d63d4425e9128d574ecdb0cb3193898943abe433
xxb <xxb@163.xom>
2022-09-06 18:01:54 +0800

    http模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 //配置路由
 const routes:Routes=[
   {path:"login",component:LoginComponent},
@@ -21,6 +22,8 @@ const routes:Routes=[
   //导入模块
   imports: [
     BrowserModule,
+    //导入网络请求模块
+    HttpClientModule,
     //使用模板驱动表单
     FormsModule,
     //导入路由模块
