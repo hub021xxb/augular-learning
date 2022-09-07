4ce60be6ddb6db96b2833c8547dd6473d9977bce
xxb <xxb@163.xom>
2022-09-07 09:40:59 +0800

    服务注入组件,并发送http请求

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//导入class类
 import {LoginUser} from "../login-user";
+import {User, UserService} from "../user.service";
+import {Result} from "../common/result";
 
 @Component({
   selector: 'app-login',
@@ -8,15 +11,27 @@ import {LoginUser} from "../login-user";
 })
 export class LoginComponent implements OnInit {
   model:LoginUser;
-  constructor() {
+
+  constructor(private userService: UserService) {
     this.model=new LoginUser();
   }
 
   ngOnInit(): void {
   }
-  login(pwd:any){
+  login(){
     //5413
-    console.log(pwd)
+    console.log(this.model);
+    //发送请求
+    this.userService.login(this.model).subscribe((result:Result<User>)=>{
+      if(result.success){
+        alert("登录成功");
+      }else{
+        alert("登录失败");
+      }
+    },(error)=>{
+      console.log(error);
+      alert("登录失败");
+    })
   }
 
 }
